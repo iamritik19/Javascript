46c622ecd812db409ab066b5dce11d933e12545b
iamritik19 <ritikraushan19@gmail.com>
2024-08-11 12:54:39 +0530

    First commit

---
+++ dateTime/customDateObject.js
@@ -0,0 +1,11 @@
+const myDate=new Date();
+const dateObj=myDate.toLocaleString('en-us',{
+    day:'2-digit',
+    month:'2-digit',
+    year:'numeric',
+    hour:'numeric',
+    minute:'numeric',
+    second:'numeric',
+    hour12:true
+});
+console.log(dateObj);
